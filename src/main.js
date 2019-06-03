43bb203642960716efc2ab4cbf2645df379f5b85
yonggandexiaobaitu <2397330983@qq.com>
2019-06-03 15:41:28 +0800

    定义了首页路由规则安装全局样式

--- src/main.js
+++ src/main.js
@@ -4,6 +4,37 @@ import App from './App.vue'
 Vue.config.productionTip = false
 // 在main.js初始化，林凯坚第一遍
 
+//1.导入全局的样式
+import './assets/statics/site/css/style.css'
+
+//2.导入index首页组件
+import index from './components/index.vue'
+
+//导入路由，先安装路由包,然后导入路由包
+import VueRouter from 'vue-router'
+//使用路由
+Vue.use(VueRouter)
+//编写路由规则,默认显示首页
+const routes = [{
+  path: '/',
+  component: index
+},
+{
+  path: '/index',
+  component: index
+},
+];
+//实例化路由对象
+let router = new VueRouter({
+  // 注入属性
+  routes
+});
+
+
+
+
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+});
